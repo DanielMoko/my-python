3798bf56d879588c1ed14fcba424da3f7e10332e
CharlesAndere <charlesandere41@gmail.com>
2024-03-18 05:39:44 -0400

    modifiedlists

--- list.py
+++ list.py
@@ -9,6 +9,18 @@ while len(name)<3:
 
 name_counter= {name [0]: 0,name[1]: 0}
 print(name)
-"""
+
 input=[input("enter ur name:"),input("enter ur name:"),input("enter ur name:")]
 print(input)
+"""
+
+fruits = ("apple","peas","tomato")
+print(fruits)
+print(fruits[2])
+#making a tuple a list
+fruitlist=list(fruits)
+print(fruitlist)
+fruitlist[2] = "greens"
+print(fruitlist)
+fruitlist=tuple(fruits)
+print(fruits)
